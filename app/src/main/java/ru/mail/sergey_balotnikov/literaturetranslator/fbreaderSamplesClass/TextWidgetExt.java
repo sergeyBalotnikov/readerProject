80d3f7a3d934e18df4efbfcbe1a76fd1108ecc17
sergeyBalotnikov <sergey.balotnikov@gmail.com>
2020-02-28 13:53:52 +0300

    reader translate in panel

--- app/src/main/java/ru/mail/sergey_balotnikov/literaturetranslator/fbreaderSamplesClass/TextWidgetExt.java
+++ app/src/main/java/ru/mail/sergey_balotnikov/literaturetranslator/fbreaderSamplesClass/TextWidgetExt.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import org.fbreader.book.Book;
 import org.fbreader.extras.info.InfoUtil;
@@ -75,10 +76,11 @@ public class TextWidgetExt extends TextWidget {
 		if (data != null) {
 			CompletableFuture.supplyAsync(() ->
 					Interpreter.translatedText(data.text()))
-					.thenAccept(s -> {
-                        Log.d("SVB", s);
-                        showTranslatePanel(this, data.rects, s, new SelectionPanelListener(this));
-                    });
+					.thenAcceptAsync(s ->{
+								Log.d("SVB", s);
+								showTranslatePanel(this, data.rects, s, new SelectionPanelListener(this));
+
+							}, ContextCompat.getMainExecutor(this.getContext()));
 			/*SelectionPanelUtil.showPanel(
 				this, data.rects, new SelectionPanelListener(this)
 			);*/
@@ -115,63 +117,61 @@ public class TextWidgetExt extends TextWidget {
 		NavigationUtil.updatePanel(this);
 	}
 	public void showTranslatePanel(@NonNull View widget, List<Rect> selectionRects, String s,  View.OnClickListener listener){
-        widget.post(new Runnable() {
-            @Override
-            public void run() {
-                final View panel = TextWidgetExt.this.findOrCreatePanel(widget);
-                if (panel == null) {
-                    return;
-                }
-                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT,
-                        RelativeLayout.LayoutParams.WRAP_CONTENT
-                );
-                layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-                final int verticalPosition;
-                if (selectionRects.isEmpty()) {
-                    verticalPosition = RelativeLayout.CENTER_VERTICAL;
-                } else {
-                    int top = Integer.MAX_VALUE;
-                    int bottom = Integer.MIN_VALUE;
-                    for (Rect r : selectionRects) {
-                        top = Math.min(top, r.top);
-                        bottom = Math.max(bottom, r.bottom);
-                    }
-                    final int spaceTop = top;
-                    final int spaceBottom = widget.getHeight() - bottom;
-                    if (spaceTop > spaceBottom) {
-                        verticalPosition = spaceTop > panel.getHeight() + 20
-                                ? RelativeLayout.ALIGN_PARENT_TOP : RelativeLayout.CENTER_VERTICAL;
-                    } else {
-                        verticalPosition = spaceBottom > panel.getHeight() + 20
-                                ? RelativeLayout.ALIGN_PARENT_BOTTOM : RelativeLayout.CENTER_VERTICAL;
-                    }
-                }
-                layoutParams.addRule(verticalPosition);
-                panel.setLayoutParams(layoutParams);
-                TextView textView = TextWidgetExt.this.findViewById(R.id.tvTranslate);
-                textView.setText(s);
-                ImageButton addBtn = TextWidgetExt.this.findViewById(R.id.btnAdd);
-                addBtn.setOnClickListener(view -> {
-                    //add translate to dictionary
+		final View panel = findOrCreatePanel(widget);
+		if (panel == null) {
+			return;
+		}
+		RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT
+		);
+		layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		final int verticalPosition;
+		if (selectionRects.isEmpty()) {
+			verticalPosition = RelativeLayout.CENTER_VERTICAL;
+		} else {
+			int top = Integer.MAX_VALUE;
+			int bottom = Integer.MIN_VALUE;
+			for (Rect r : selectionRects) {
+				top = Math.min(top, r.top);
+				bottom = Math.max(bottom, r.bottom);
+			}
+			final int spaceTop = top;
+			final int spaceBottom = widget.getHeight() - bottom;
+			if (spaceTop > spaceBottom) {
+				verticalPosition = spaceTop > panel.getHeight() + 20
+						? RelativeLayout.ALIGN_PARENT_TOP : RelativeLayout.CENTER_VERTICAL;
+			} else {
+				verticalPosition = spaceBottom > panel.getHeight() + 20
+						? RelativeLayout.ALIGN_PARENT_BOTTOM : RelativeLayout.CENTER_VERTICAL;
+			}
+		}
+		layoutParams.addRule(verticalPosition);
+		panel.setLayoutParams(layoutParams);
+		TextView textView = panel.findViewById(R.id.tvTranslate);
+		textView.setText(s);
+		ImageButton addBtn = panel.findViewById(R.id.btnAdd);
+		/*addBtn.setOnClickListener(view -> {
+			//add translate to dictionary
 
-                });
-                panel.setVisibility(View.VISIBLE);
-                panel.requestFocus();
-            }
-        });
+		});*/
+		panel.setVisibility(View.VISIBLE);
+		panel.requestFocus();
     }
 
     private View findOrCreatePanel(View widget) {
-        final ViewParent parent = widget.getParent();
-        if (!(parent instanceof RelativeLayout)) {
-            return null;
-        }
+		final ViewParent parent = widget.getParent();
+		if (!(parent instanceof RelativeLayout)) {
+			return null;
+		}
 
-        final RelativeLayout root = (RelativeLayout)parent;
-        return ViewUtil.findView(root, R.id.selection_panel, () -> {
-            root.inflate(getContext(), R.layout.layout_selection_translate_pannel, root);
-            return ViewUtil.findView(root, R.id.selection_panel);
-        });
+		final RelativeLayout root = (RelativeLayout)parent;
+		return ViewUtil.findView(root, R.id.selection_panel, new ViewUtil.Supplier<View>() {
+			@Override
+			public View get() {
+				root.inflate(root.getContext(), R.layout.layout_selection_translate_pannel, root);
+				return ViewUtil.findView(root, R.id.selection_panel);
+			}
+		});
     }
 }
